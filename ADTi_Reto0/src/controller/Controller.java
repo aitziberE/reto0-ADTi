b6b95ee2a581268ed942e9fcff764cc747c79ceb
AnderMoreno15 <amorenoch15@gmail.com>
2024-09-24 09:02:32 +0200

    Controller changed

--- ADTi_Reto0/src/controller/Controller.java
+++ ADTi_Reto0/src/controller/Controller.java
@@ -5,10 +5,44 @@
  */
 package controller;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
 /**
  *
  * @author Ander
  */
 public class Controller {
+
+    private Connection con;
+    private PreparedStatement stmt;
+    private ConnectionOpenClose conection = new ConnectionOpenClose();
+
+    /*
+    crearUnidadDidactica
+    crearConvocatoria
+    crearEnunciado
+
+    consultarEnunciado
+    consultarConvocatoriaPorEnunciado
+    consultarDescripcionEnunciado
+
+    asignarEnunciadoAConvocatoria
+     */
+    /**
+     * Crear una unidad didáctica (Unidad) y convocatoria (Convocatoria) de examen.
+     */
+    final String INSERTunidaddidactica = "INSERT INTO UnidadDidactica VALUES (?, ?, ?, ?, ?)";
+    final String INSERTconvocatoria = "INSERT INTO ConvocatoriaExamen VALUES (?, ?, ?, ?, ?)";
     
+    /**
+     * Crear un enunciado de examen agregando las unidades didácticas que va a referir. 
+     * También se asociará a este enunciado la convocatoria para la que se crea.
+     */
+    final String INSERTenunciado = "INSERT INTO Enunciado VALUES (?, ?, ?, ?, ?)";
+
+    final String SELECTenunciado = "SELECT * FROM Enunciado WHERE username = ? AND contrasena = ?";
+
+    final String UPDATEcoleccion = "UPDATE Coleccion SET armaFav = ?, skinFav = ?, agenteFav = ? WHERE dni_jugador = ?";
+
 }
