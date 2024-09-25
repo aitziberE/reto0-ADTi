4e742e008cbc9a4b48b7efcf6f6e826d43f3190e
aitziberE <eskizabel.aitziber@gmail.com>
2024-09-25 20:19:54 +0200

    add validation to Dificultad class

--- ADTi_Reto0/src/model/Dificultad.java
+++ ADTi_Reto0/src/model/Dificultad.java
@@ -4,11 +4,39 @@
  * and open the template in the editor.
  */
 package model;
+import utilidades.Util;
 
 /**
  *
- * @author Ander
+ * @author Ander, Aitziber
  */
 public enum Dificultad {
-    ALTA, MEDIA, BAJA
+    ALTA, MEDIA, BAJA;
+    
+    /**
+     *
+     * @return
+     */
+    public Dificultad validarDificultad() {
+        String str;
+        Dificultad dificultad = Dificultad.BAJA;
+        boolean error;
+        do {
+            str = Util.introducirCadena("Introduzca dificultad: (ALTA - MEDIA - BAJA)");
+            if (str != null) {
+                try {
+                    dificultad = Dificultad.valueOf(str.toUpperCase());
+                    error = false;
+                } catch (IllegalArgumentException e) {
+                    System.out.println("Dificultad no válida. Por favor, introduzca uno de los valores permitidos.");
+                    error = true;
+                }
+            } else {
+                System.out.println("La entrada no puede estar vacía. Por favor, introduzca un valor.");
+                error = true;
+            }
+        } while (error);
+        return dificultad;
+    }
+    
 }
